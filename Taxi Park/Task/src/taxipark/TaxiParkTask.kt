aee46ca73853b2e84735d0433c61ca955f5c8c76
Ricardo Costa <ricardocosta101085@gmail.com>
2023-08-07 10:50:41 +0100

    feat: getting the smart passengers by using filters associateWiths, counts and flat maps.

--- Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -31,8 +31,41 @@ fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> {
 /*
  * Task #4. Find the passengers who had a discount for majority of their trips.
  */
-fun TaxiPark.findSmartPassengers(): Set<Passenger> =
-    TODO()
+fun TaxiPark.findSmartPassengers(): Set<Passenger> {
+    val countTripsPerPassenger =
+        allPassengers.associateWith { passenger -> trips.count { trip -> trip.passengers.contains(passenger) } }
+    val passengersHadDiscounts =
+        trips.filter { trip -> (trip.discount ?: 0.0) > 0.0 }.flatMap { trip -> trip.passengers }
+            .toSet()
+    val tripsWithDiscounts = trips.filter { (it.discount ?: 0.0) > 0.0 }
+    val countTripsWithDiscountPerPassenger = passengersHadDiscounts.associateWith { passenger ->
+        tripsWithDiscounts.count { trip ->
+            trip.passengers.contains(
+                passenger
+            )
+        }
+    }
+    //println("count trips per passenger: $countTripsPerPassenger")
+    //println("passenger that had discounts: $passengersHadDiscounts")
+    //println("count trips with discount per passenger: $countTripsWithDiscountPerPassenger")
+    return allPassengers.filter { passenger ->
+        (countTripsWithDiscountPerPassenger[passenger] ?: 0).toDouble()
+            .div(countTripsPerPassenger[passenger]!!) > 0.5
+    }.toSet()
+}
+
+fun TaxiPark.findSmartPassengers2(): Set<Passenger> {
+    val tripsWithDiscounts = trips.filter { (it.discount ?: 0.0) > 0.0 }
+    val countTripsWithDiscountPerPassenger = tripsWithDiscounts
+        .flatMap { it.passengers }
+        .groupingBy { it }
+        .eachCount()
+
+    return allPassengers.filter { passenger ->
+        countTripsWithDiscountPerPassenger.getOrDefault(passenger, 0) * 2 >
+                trips.count { it.passengers.contains(passenger) }
+    }.toSet()
+}
 
 /*
  * Task #5. Find the most frequent trip duration among minute periods 0..9, 10..19, 20..29, and so on.
